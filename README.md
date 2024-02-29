aa3567da14c3cb981c8e95ed7ef63a10527bc800
enzo-viguier <enzoviguier@outlook.fr>
2024-02-29 08:22:22 +0100

    Installation de Tailwind avec AlpineJS

---
+++ README.md
@@ -0,0 +1,5 @@
+# Commande importante :
+
+symfony console tailwind:build --watch
+
+Check le site : https://www.hyperui.dev/
--- importmap.php
+++ importmap.php
@@ -27,4 +27,7 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'alpinejs' => [
+        'version' => '3.13.5',
+    ],
 ];
